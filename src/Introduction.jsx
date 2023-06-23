2532b55a3ffac56d8163e166b7c8ce11de00136a
PkTheCoda <pranavkonjeti@gmail.com>
2023-06-23 14:11:26 -0400

    Added introduction section

--- src/Introduction.jsx
+++ src/Introduction.jsx
@@ -6,6 +6,14 @@ function Introduction() {
         <>
             <section className="introduction--wrapper">
                 <Header />
+                <div className="introduction-info-holder">
+                    <h1 className="intro--title">Hi There, I'm Pranav 👋</h1>
+                    <h3 className="intro--subtitle">I'm a highschooler with a <span className="tc">passion for web-development</span></h3>
+                    <div className="intro-button-holder">
+                        <button className="intro--button">About Me</button>
+                        <button className="intro--button">My Work</button>
+                    </div>
+                </div>
             </section>
         </>
     )
