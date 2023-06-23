8d6b0f19c98d4f77a2d946b6e1eeb9b4dfb07bbc
PkTheCoda <pranavkonjeti@gmail.com>
2023-06-23 15:13:50 -0400

    Started "skills" section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Introduction from './Introduction';
 import About from './About';
+import Skills from './Skills';
 
 function App() {
   return (
     <>
       <Introduction />
       <About />
+      <Skills />
     </>
   );
 }
---
+++ src/Skills.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGitHub } from "@fortawesome/free-regular-svg-icons";
+
+function Skills() {
+    return (
+        <section className="skills--wrapper">
+            <h1 className="skills--title"> <span className="checkpoint-num">02</span> My Skills</h1>
+            <div className="skills--holder">                
+               
+            
+                    
+            </div>
+        </section>
+    )
+}
+
+export default Skills
