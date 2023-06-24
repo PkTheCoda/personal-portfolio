4a727f34cbf70af579ec0ae8a0b52db52f56ebda
PkTheCoda <pranavkonjeti@gmail.com>
2023-06-24 17:56:54 -0400

    Added introduction animation

--- src/Components/Introduction.jsx
+++ src/Components/Introduction.jsx
@@ -1,22 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Header from "./Header";
 
 function Introduction() {
-    return (
-        <>
-            <section className="introduction--wrapper">
-                <Header />
-                <div className="introduction-info-holder">
-                    <h1 className="intro--title">Hi There, I'm Pranav 👋</h1>
-                    <h3 className="intro--subtitle">I'm a highschooler with a <span className="tc">passion for web-development</span></h3>
-                    <div className="intro-button-holder">
-                        <button className="intro--button"><a href="#about">About Me</a></button>
-                        <button className="intro--button"><a href="#projects">My Work</a></button>
-                    </div>
-                </div>
-            </section>
-        </>
-    )
+
+  return (
+    <>
+      <section className="introduction--wrapper">
+        <Header />
+        <div className="introduction-info-holder">
+          <h1 className="intro--title">Hi There, I'm Pranav 👋</h1>
+          <h3 className="intro--subtitle">I'm a highschooler with a <span className="tc">passion for web-development</span></h3>
+          <div className="intro-button-holder">
+            <button className="intro--button"><a href="#about">About Me</a></button>
+            <button className="intro--button"><a href="#projects">My Work</a></button>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+  
 }
 
-export default Introduction
+export default Introduction;
