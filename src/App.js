f2a36d493ae3239d0034a8cce4bfad88d41713d0
PkTheCoda <pranavkonjeti@gmail.com>
2023-06-23 23:36:05 -0400

    Begun adding "projects" section of portfolio

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
-import Introduction from './Introduction';
-import About from './About';
-import Skills from './Skills';
+import Introduction from './Components/Introduction';
+import About from './Components/About';
+import Skills from './Components/Skills';
+import Projects from './Components/Projects';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Introduction />
       <About />
       <Skills />
+      <Projects />
     </>
   );
 }
---
+++ src/Components/Projects.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Projects() {
+    return (
+        <>
+            <h1 className="projects--title"> <span className="checkpoint-num">03</span> My <span className="tc">Projects</span></h1>
+            <section className="projects--wrapper">
+                
+            </section>
+        </>
+    )
+}
+
+export default Projects
