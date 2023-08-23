230f3bb56ab908930ff9c8c46d02f6ea736fb3e0
PkTheCoda <pranavkonjeti@gmail.com>
2023-08-22 20:25:41 -0400

    added footer message

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,7 +3,12 @@ import React from "react";
 function Footer() {
     return (
         <section className="footer--wrapper">
-            <h1 className="footer-title">Made with ❤️ (and a lot of googling) by Pranav</h1>
+            <h1 className="footer-title" style={{textAlign: "center"}}>
+                Made with ❤️ (and a lot of googling) by Pranav
+                <br />
+                This portfolio is <b>no longer in development.</b> Checkout the new one <span style={{color: "blue", fontWeight: "700"}}><a href="https://pranavk.vercel.app" target="_blank">here</a></span>
+            </h1>
+            
         </section>
     )
 }
